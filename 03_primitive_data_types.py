9aa643b0dec4a2e1260a9f2de543232bc300e732
Chris Ardon <rockrisbl@gmail.com>
2021-11-28 02:02:32 -0600

    example 03 added

---
+++ 03_primitive_data_types.py
@@ -0,0 +1,19 @@
+#Data Types
+
+#String
+
+print("Hello"[4]) #Print the letter with index 4
+
+#Integer
+
+print(123 + 345)
+123_456_789 #We can write the numbers in this form, so it is easier to read the units
+
+#Float
+
+3.14159
+
+#Boolean
+
+True
+False
