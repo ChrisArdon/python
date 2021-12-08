0a48bc6086cf00b6aa50e497936097a9fdfa7b57
Chris Ardon <rockrisbl@gmail.com>
2021-12-07 23:55:44 -0600

    06 added

---
+++ 06_mathematical operators.py
@@ -0,0 +1,10 @@
+3 + 5 #addition
+7 - 4 #substraction
+3 * 2 #multiplication
+6 / 3 #division
+2 ** 3 #raise to a power
+
+##PEMDASLR () ** */ +-
+#Parenthesis, Exponential, Multiplication and Division, Addition and Substraction, in an order from Left to Right
+
+print(3 * 3 + 3 / 3 - 3)
