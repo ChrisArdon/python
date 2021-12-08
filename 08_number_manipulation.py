513267caa8252f23f031aa9335c21396b7cae4ee
Chris Ardon <rockrisbl@gmail.com>
2021-12-08 01:22:27 -0600

    08 added

---
+++ 08_number_manipulation.py
@@ -0,0 +1,14 @@
+print(8/3) #Original output
+print(int(8/3)) #int cut the decimal part, returning only the integer
+print(round(8/3)) #it rounds the number
+print(round(8/3, 2)) #round it to two decimal places
+print(8//3) #integer division operator
+
+#Using the same variable with multiple operations
+score = 0
+print(score)
+score += 1
+print(score)
+score += 1
+print(score)
+
