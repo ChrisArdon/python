4611f6e8ed7834e6f6f9e803b9629ce9e59109f2
Chris Ardon <rockrisbl@gmail.com>
2021-12-08 01:48:07 -0600

    10 added

---
+++ 10_Life_in_weeks.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+daysLeft = (90 - int(age))*365
+weeksLeft = (90 - int(age))*52
+monthsLeft = (90 - int(age))*12
+print(f"You have {daysLeft} days, {weeksLeft} weeks, and {monthsLeft} months left.")
