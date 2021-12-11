44eaeb731042f11891420fff2b4637f7aba6b3c0
Chris Ardon <rockrisbl@gmail.com>
2021-12-10 23:41:19 -0600

    17 added

---
+++ 17_multiple_if_statements_in_succession.py
@@ -0,0 +1,24 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    print("Child tickets are $5.")
+    bill = 5
+  elif age <= 18:
+    print("Youth tickets are $7.")
+    bill = 7
+  else:
+    print("Adult tickets are $12.")
+    bill = 12
+
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3 #bill = bill + 3
+
+  print(f"Your final bill is {bill}")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
