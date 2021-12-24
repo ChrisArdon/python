4ef58698320131d1f69f571b40bad99c5d45edc7
Chris Ardon <rockrisbl@gmail.com>
2021-12-24 07:40:38 -0600

    26 added

---
+++ 26_lists.py
@@ -0,0 +1,15 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire"]
+
+#Print an element from the list given the index
+print(states_of_america[3])
+
+#We can also use negative indexes, which starts from the end
+print(states_of_america[-1])
+
+#We can modify an element from the list
+states_of_america[1] = "Pencilvania"
+
+#Add a new object to the end of a list
+states_of_america.append("Andrea Elias Island")
+
+print(states_of_america)
