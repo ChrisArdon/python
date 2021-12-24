6d58b096adb4ba6c2b5717345d8e1149440e4866
Chris Ardon <rockrisbl@gmail.com>
2021-12-24 06:53:06 -0600

    24 added

---
+++ 24_random_numbers.py
@@ -0,0 +1,13 @@
+import random
+
+print("Printing a random integer:")
+random_integer = random.randint(1,20)
+print(random_integer)
+
+print("Printing a random float, between [0,1)")
+random_float = random.random()
+print(random_float)
+
+print("Printing a random number between 0 - 5")
+random_number = random.random() * 5
+print(random_number)
