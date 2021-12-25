5995dadac94d9f5bf0456a11ac9392b5bc15e9ca
Chris Ardon <rockrisbl@gmail.com>
2021-12-25 07:13:14 -0600

    32 added

---
+++ 32_average_height_using_for.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+total_height = 0
+count = 0
+for student in student_heights:
+    total_height += student
+    count += 1
+
+avg = round(total_height/count)
+print(avg)
