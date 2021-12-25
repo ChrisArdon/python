9adb405ab2aa3c4672fcb984ca789d1484e0ad34
Chris Ardon <rockrisbl@gmail.com>
2021-12-25 10:17:44 -0600

    36 37 added

---
+++ 36_PythonLoops.py
@@ -0,0 +1,17 @@
+#For loop with Lists
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+  print(fruit)
+
+
+#For Loop with Range
+for number in range(1, 10, 4):
+  print(number)
+
+#Adding all the numbers from 1 to 100
+total = 0
+for number in range(1,101):
+  total += number
+
+print(total)
