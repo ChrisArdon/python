5b88df6174ec48e92dc384c55f67891434173440
Chris Ardon <rockrisbl@gmail.com>
2022-02-23 05:10:45 -0600

    notes

--- Day 12 - Beginner - Scope & Number Guessing Game/01 - Namespaces Local vs Global Scope.py
+++ Day 12 - Beginner - Scope & Number Guessing Game/01 - Namespaces Local vs Global Scope.py
@@ -53,3 +53,31 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}") #Prints "Skeleton"
+
+
+######### Modifying Global Scope #########
+enemies = 1
+def increase_enemies():
+    global enemies  #If we want to modify de goblal variable in a local enviroment
+    enemies += 1
+    print(f"enemies inside function: {enemies}")
+    
+    
+######### Using the Global variable without modifying it localy #########
+enemies = 1
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1 #We use a return to modify the variable localy and if we want to keep the goblal value
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+
+
+############### Global Constants ###############
+#We can use global variables as a constants
+#They can be written in Uppercase to differenciate them from normal variables.
+PI = 3.14159
+URL = "https://www.google.com"
+TWITTER_HANDLE = "@ChrizHellscream"
