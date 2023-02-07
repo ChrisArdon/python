109713037f553cd84603f39560855227b443fd00
Chris Ardon <79287174+ChrisArdon@users.noreply.github.com>
2023-02-06 22:24:12 -0600

    Update 39_Functions_with_more_than_1_input_and_keywords.py

--- Day 08 - Beginner - Function Parameters & Caesar Cipher/39_Functions_with_more_than_1_input_and_keywords.py
+++ Day 08 - Beginner - Function Parameters & Caesar Cipher/39_Functions_with_more_than_1_input_and_keywords.py
@@ -2,9 +2,9 @@
 def greet_with(name, location):
     print(f"Hello {name}")
     print(f"What is it like in {location}")
-greet_with("Andrea", "San Salvador")
+greet_with("Chris", "San Salvador")
 
 print("______")
 
 #Functions with keyword arguments
-greet_with(name="Gisselle", location="Altavista") #Here the location doesn't matters
+greet_with(name="Chris", location="San Salvador") #Here the location doesn't matters
